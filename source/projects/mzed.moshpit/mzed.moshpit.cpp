8afb0630810773bc34a5f297eb9f883a28a0de15
mzed <github@mikezed.com>
2021-05-09 11:43:45 +0100

    drawing with rect

--- source/projects/mzed.moshpit/mzed.moshpit.cpp
+++ source/projects/mzed.moshpit/mzed.moshpit.cpp
@@ -105,7 +105,7 @@ public:
             
             rect<fill> {	// background
                 t,
-                color { 0.5, 0.5, 0, 1.0 }
+                color { 0.9, 0.9, 0.9, 1.0 }
             };
 
 
@@ -213,8 +213,7 @@ public:
                  color{ 0.0, 1.0, 0.0, 1.0 },
                  position { 100, 100 },
                  size { 10, 10 },
-                 line_width { 1.0 }
-
+                 line_width { 1.0 },
              };
 
 
