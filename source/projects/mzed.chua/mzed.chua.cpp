323e302418805f96cea10a0797373a1b3c4e56a1
mzed <github@mikezed.com>
2025-08-11 14:48:20 +0100

    Formatting
    # Conflicts:
    #	source/projects/mzed.chua/mzed.chua.cpp
    #	source/projects/mzed.lorenz/mzed.lorenz.cpp

--- source/projects/mzed.chua/mzed.chua.cpp
+++ source/projects/mzed.chua/mzed.chua.cpp
@@ -10,110 +10,67 @@ using namespace c74::min;
 class mzed_chua : public object<mzed_chua>
 {
 public:
-    MIN_DESCRIPTION { "Generate a Chua attractor." };
-    MIN_TAGS        { "chaos" };
-    MIN_AUTHOR      { "mzed" };
-    MIN_RELATED     { "roessler, lorenz" };
+  MIN_DESCRIPTION { "Generate a Chua attractor." };
+  MIN_TAGS        { "chaos" };
+  MIN_AUTHOR      { "mzed" };
+  MIN_RELATED     { "roessler, lorenz" };
+  
+  inlet<>  input    { this, "(bang) calculate next point on attractor" };
+  outlet<> outlet_x { this, "(float) x coordinate" };
+  outlet<> outlet_y { this, "(float) y coordinate" };
+  outlet<> outlet_z { this, "(float) z coordinate" };
 
-    inlet<>  input{ this, "(bang) calculate next point on attractor" };
-    outlet<> outlet_x{ this, "(float) x coordinate" };
-    outlet<> outlet_y{ this, "(float) y coordinate" };
-    outlet<> outlet_z{ this, "(float) z coordinate" };
+  attribute<double> c_a { this, "a", 14.5 };
+  attribute<double> c_b { this, "b", 1.0 };
+  attribute<double> c_c { this, "c", 25.58 };
+  attribute<double> c_d { this, "d", -1.0 }; // -8/7 -1.14285714286
+  attribute<double> c_e { this, "e", 0.0 }; // -5/7 -0.714285714286
+  attribute<double> c_h { this, "timestep (h)", 0.01 };
 
-    attribute<double> c_a{ this, "a", 14.5 };
-    attribute<double> c_b{ this, "b", 1.0 };
-    attribute<double> c_c{ this, "c", 25.58 };
-    attribute<double> c_d{ this, "d", -1.0 }; // -8/7 -1.14285714286
-    attribute<double> c_e{ this, "e", 0.0 }; // -5/7 -0.714285714286
-    attribute<double> c_h{ this, "timestep (h)", 0.01 };
+  argument<number> a_arg { this, "a", "Initial a value.", MIN_ARGUMENT_FUNCTION { c_a = arg; } };
+  argument<number> b_arg { this, "b", "Initial b value.", MIN_ARGUMENT_FUNCTION { c_b = arg; } };
+  argument<number> c_arg { this, "c", "Initial c value.", MIN_ARGUMENT_FUNCTION { c_c = arg; } };
+  argument<number> d_arg { this, "d", "Initial d value.", MIN_ARGUMENT_FUNCTION { c_d = arg; } };
+  argument<number> e_arg { this, "e", "Initial e value.", MIN_ARGUMENT_FUNCTION { c_e = arg; } };
+  argument<number> h_arg { this, "h", "Initial h (timestep) value.", MIN_ARGUMENT_FUNCTION { c_h = arg; } };
 
-    argument<number> a_arg
+  message<> bang
+  {
+    this, "bang", "Calculate the next point.",
+    MIN_FUNCTION
     {
-      this, "a", "Initial a value.",
-      MIN_ARGUMENT_FUNCTION
-      {
-        c_a = arg;
-      }
-    };
-
-    argument<number> b_arg
+      // calculate the attractor
+      const double zNew { currentZ + (c_h * -c_c * currentY) };
+      const double yNew { currentY + (c_h * c_b * (currentX - currentY + currentZ)) };
+      const double g    { (c_e * currentX) + (c_d + c_e) * (fabs(currentX + 1) - fabs(currentX - 1)) };
+      const double xNew { currentX + (c_h * c_a * (currentY - currentX - g)) };
+      
+      // save the calculated values
+      currentX = xNew;
+      currentY = yNew;
+      currentZ = zNew;
+      
+      // output the calculated values
+      outlet_z.send(zNew);
+      outlet_y.send(yNew);
+      outlet_x.send(xNew);
+      
+      return {};
+    }
+  };
+  
+  
+  // post to max window == but only when the class is loaded the first time
+  message<> maxclass_setup
+  {
+    this, "maxclass_setup",
+    MIN_FUNCTION
     {
-      this, "b", "Initial b value.",
-      MIN_ARGUMENT_FUNCTION
-      {
-        c_b = arg;
-      }
-    };
-
-    argument<number> c_arg
-    {
-      this, "c", "Initial c value.",
-      MIN_ARGUMENT_FUNCTION
-      {
-        c_c = arg;
-      }
-    };
-
-    argument<number> d_arg
-    {
-      this, "d", "Initial d value.",
-      MIN_ARGUMENT_FUNCTION
-      {
-        c_d = arg;
-      }
-    };
-
-    argument<number> e_arg
-    {
-      this, "e", "Initial e value.",
-      MIN_ARGUMENT_FUNCTION
-      {
-        c_e = arg;
-      }
-    };
-
-    argument<number> h_arg
-    { this, "h", "Initial h (timestep) value.",
-      MIN_ARGUMENT_FUNCTION
-      {
-        c_h = arg;
-      }
-    };
-
-    message<> bang
-    {
-      this, "bang", "Calculate the next point.",
-      MIN_FUNCTION
-      {
-            // calculate the attractor
-            const double zNew { currentZ + (c_h * -c_c * currentY) };
-            const double yNew { currentY + (c_h * c_b * (currentX - currentY + currentZ)) };
-            const double g { (c_e * currentX) + (c_d + c_e) * (fabs(currentX + 1) - fabs(currentX - 1)) };
-            const double xNew { currentX + (c_h * c_a * (currentY - currentX - g)) };
-
-            // save the calculated values
-            currentX = xNew;
-            currentY = yNew;
-            currentZ = zNew;
-
-            // output the calculated values
-            outlet_z.send(zNew);
-            outlet_y.send(yNew);
-            outlet_x.send(xNew);
-            return {};
-          }
-    };
-    // post to max window == but only when the class is loaded the first time
-    message<> maxclass_setup
-    {
-        this, "maxclass_setup",
-        MIN_FUNCTION
-        {
-            cout << "mzed.chua object by Michael F. Zbyszyński, v3.0 ©2002-2024" << endl;
-            return {};
-        }
-    };
-
+      cout << "mzed.chua object by Michael F. Zbyszyński, v1.0 (c)2022-2025" << endl;
+      return {};
+    }
+  };
+  
 private:
     double currentX { 1.0 };
     double currentY { 1.0 };
--- source/projects/mzed.lorenz/mzed.lorenz.cpp
+++ source/projects/mzed.lorenz/mzed.lorenz.cpp
@@ -10,111 +10,86 @@ using namespace c74::min;
 class mzed_lorenz : public object<mzed_lorenz>
 {
 public:
-    MIN_DESCRIPTION { "Generate a Lorenz attractor." };
-    MIN_TAGS        { "chaos" };
-    MIN_AUTHOR      { "mzed" };
-    MIN_RELATED     { "roessler, chua" };
+  MIN_DESCRIPTION { "Generate a Lorenz attractor." };
+  MIN_TAGS { "chaos" };
+  MIN_AUTHOR { "mzed" };
+  MIN_RELATED { "roessler, chua" };
 
-    inlet<>  inlet_x{ this, "(float or bang) bang calculates or x coordinate of previous point in attractor" };
-    inlet<>  inlet_y{ this, "(float) y coordinate of previous point in attractor" };
-    inlet<>  inlet_z{ this, "(float) z coordinate of previous point in attractor" };
-    inlet<>  inlet_h{ this, "(float) timestep (h) value for attractor" };
+  inlet<>  inlet_x { this, "(float or bang) bang calculates or x coordinate of previous point in attractor" };
+  inlet<>  inlet_y { this, "(float) y coordinate of previous point in attractor" };
+  inlet<>  inlet_z { this, "(float) z coordinate of previous point in attractor" };
+  inlet<>  inlet_h { this, "(float) timestep (h) value for attractor" };
 
-    outlet<> outlet_x{ this, "(float) x coordinate" };
-    outlet<> outlet_y{ this, "(float) y coordinate" };
-    outlet<> outlet_z{ this, "(float) z coordinate" };
+  outlet<> outlet_x { this, "(float) x coordinate" };
+  outlet<> outlet_y { this, "(float) y coordinate" };
+  outlet<> outlet_z { this, "(float) z coordinate" };
 
-    attribute<double> l_h{ this, "timestep (h)", 0.01 };
+  attribute<double> l_h { this, "timestep (h)", 0.01 };
+  
+  argument<number> x_arg { this, "x", "Initial x value.", MIN_ARGUMENT_FUNCTION { currentX = arg; } };
+  argument<number> y_arg { this, "y", "Initial y value.", MIN_ARGUMENT_FUNCTION { currentY = arg; } };
+  argument<number> z_arg { this, "z", "Initial z value.", MIN_ARGUMENT_FUNCTION { currentZ = arg; } };
 
-    argument<number> x_arg
+  argument<number> h_arg { this, "h", "Initial h (timestep) value.", MIN_ARGUMENT_FUNCTION { l_h = arg; } };
+
+  message<> ints
+  {
+    this, "float", "set variable",
+    MIN_FUNCTION
     {
-      this, "x", "Initial x value.",
-      MIN_ARGUMENT_FUNCTION
+      switch (inlet)
       {
-        currentX = arg;
+        case 0:
+          currentX = args[0];
+          return {};
+        case 1:
+          currentY = args[0];
+          return {};
+        case 2:
+          currentZ = args[0];
+          return {};
+        default:
+          assert(false);
       }
-    };
 
-    argument<number> y_arg
-    {
-      this, "y", "Initial y value.",
-      MIN_ARGUMENT_FUNCTION
-      {
-        currentY = arg;
-      }
-    };
-
-    argument<number> z_arg
-    {
-    this, "z", "Initial z value.",
-        MIN_ARGUMENT_FUNCTION
-    {
-      currentZ = arg;
+      return {};
     }
-    };
-
-    argument<number> h_arg
-    { this, "h", "Initial h (timestep) value.",
-      MIN_ARGUMENT_FUNCTION
-      {
-        l_h = arg;
-      }
-    };
-
-    message<> ints
+  };
+  
+  message<> bang
+  {
+    this, "bang", "Calculate the next point.",
+    MIN_FUNCTION
     {
-      this, "float", "set variable",
-      MIN_FUNCTION
-      {
-        switch (inlet)
-        {
-          case 0:
-            currentX = args[0];
-          case 1:
-            currentY = args[0];
-          case 2:
-            currentZ = args[0];
-          default:
-            assert(false);
-        }
-        return {};
-      }
-    };
-
-    message<> bang
+      // calculate the attractor
+      const double zNew { currentZ + (l_h * ((currentX * currentY) - (LorenzC * currentZ))) };
+      const double yNew { currentY + (l_h * ((LorenzA * currentX - currentY) - (currentX * currentZ))) };
+      const double xNew { currentX + ((l_h * LorenzB) * (currentY - currentX)) };
+      
+      // save the calculated values
+      currentX = xNew;
+      currentY = yNew;
+      currentZ = zNew;
+      
+      // output the calculated values
+      outlet_z.send(zNew);
+      outlet_y.send(yNew);
+      outlet_x.send(xNew);
+      return {};
+    }
+  };
+  
+  
+  // post to max window == but only when the class is loaded the first time
+  message<> maxclass_setup
+  {
+    this, "maxclass_setup", MIN_FUNCTION
     {
-      this, "bang", "Calculate the next point.",
-      MIN_FUNCTION
-      {
-            // calculate the attractor
-            const double zNew { currentZ + (l_h * ((currentX * currentY) - (LorenzC * currentZ))) };
-            const double yNew { currentY + (l_h * ((LorenzA * currentX - currentY) - (currentX * currentZ))) };
-            const double xNew { currentX + ((l_h * LorenzB) * (currentY - currentX)) };
-
-            // save the calculated values
-            currentX = xNew;
-            currentY = yNew;
-            currentZ = zNew;
-
-            // output the calculated values
-            outlet_z.send(zNew);
-            outlet_y.send(yNew);
-            outlet_x.send(xNew);
-            return {};
-          }
-    };
-
-    // post to max window == but only when the class is loaded the first time
-    message<> maxclass_setup
-    {
-        this, "maxclass_setup",
-        MIN_FUNCTION
-        {
-            cout << "lorenz object by Michael F. Zbyszyński, v3.0 ©2002-2024" << endl;
-            return {};
-        }
-    };
-
+      cout << "mzed.lorenz object by Michael F. Zbyszyński, v3.0 ©2002-2025" << endl;
+      return {};
+    }
+  };
+  
 private:
     double currentX{ 0.6 };
     double currentY{ 0.6 };
